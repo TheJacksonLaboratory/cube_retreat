938d35899f5eaba97bb88d963addc641068b505d
Hongping Liang <hongping.liang@jax.org>
2020-11-16 11:43:04 -0500

    init

---
+++ Use_Case_II/attic/allAdVariants_11.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -login
+#PBS -l walltime=24:00:00,nodes=1:ppn=1,mem=8gb
+#PBS -M annat.haber@jax.org
+#PBS -m abe
+#PBS -j oe
+
+cd ${PBS_O_WORKDIR} 
+
+module load bcftools/1.3.1
+#module load vcftools/0.1.12a
+
+InPath=/sdata/carter-lab/carter/AMPAD/WGS_PrelimProcessing/output/Joint/MAF05
+inputvcf=NIA_JG_1898_samples_GRM_WGS_b37_JointAnalysis01_2017-12-08_11.recalibrated_variants.vcf.gz
+
+bcftools query -f '[%CHROM\t%POS\t%REF\t%ALT\t%SAMPLE\t%GT\n]' -i 'POS=47416746' $InPath/$inputvcf >> \
+                $InPath/ADvariants/allADVariantsApril2020allSamples_chr11.txt
+
+bcftools query -f '[%CHROM\t%POS\t%REF\t%ALT\t%SAMPLE\t%GT\n]' -i 'POS=121435587' $InPath/$inputvcf >> \
+                $InPath/ADvariants/allADVariantsApril2020allSamples_chr11.txt
---
+++ Use_Case_II/attic/allAdVariants_6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -login
+#PBS -l walltime=24:00:00,nodes=1:ppn=1,mem=8gb
+#PBS -M annat.haber@jax.org
+#PBS -m abe
+#PBS -j oe
+
+cd ${PBS_O_WORKDIR} 
+
+module load bcftools/1.3.1
+#module load vcftools/0.1.12a
+
+InPath=/sdata/carter-lab/carter/AMPAD/WGS_PrelimProcessing/output/Joint/MAF05
+inputvcf=NIA_JG_1898_samples_GRM_WGS_b37_JointAnalysis01_2017-12-08_6.recalibrated_variants.vcf.gz
+
+
+bcftools query -f '[%CHROM\t%POS\t%REF\t%ALT\t%SAMPLE\t%GT\n]' -i 'POS=32578530' $InPath/$inputvcf >> \
+                $InPath/ADvariants/allADVariantsApril2020allSamples_chr6.txt
+
+bcftools query -f '[%CHROM\t%POS\t%REF\t%ALT\t%SAMPLE\t%GT\n]' -i 'POS=41129207' $InPath/$inputvcf >> \
+                $InPath/ADvariants/allADVariantsApril2020allSamples_chr6.txt
+
+bcftools query -f '[%CHROM\t%POS\t%REF\t%ALT\t%SAMPLE\t%GT\n]' -i 'POS=41129252' $InPath/$inputvcf >> \
+                $InPath/ADvariants/allADVariantsApril2020allSamples_chr6.txt
+
+bcftools query -f '[%CHROM\t%POS\t%REF\t%ALT\t%SAMPLE\t%GT\n]' -i 'POS=47447041' $InPath/$inputvcf >> \
+                $InPath/ADvariants/allADVariantsApril2020allSamples_chr6.txt
